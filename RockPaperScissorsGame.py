da45dc6f00b926697ca0808eb3932359ec2a43a5
Ashya <128183143+ashyapeachie@users.noreply.github.com>
2025-05-19 10:29:04 -0400

    objective and input

--- RockPaperScissorsGame.py
+++ RockPaperScissorsGame.py
@@ -1,1 +1,19 @@
-# creation of file for game
+"""
+author: Ashya#
+filename: RockPaperScissorsGame.py
+objective: Create a game where the player can play against the computer by choosing rock, 
+ paper or scissors, and the computer randomly selects a choice
+"""
+import random
+
+#taking the user's input
+def user_input_choice(): 
+    #code inside the function
+    choice = input("Please enter your choice (rock, paper, scissors): ")
+    while choice not in []: 
+    #if the user choices something that's not a choice, program will give invalid
+
+#randomized computer input
+
+#
+
